fe0e686010ba99fb9f05712ee4d4acf5ea39f50b
jlswbs <jlswbs@users.noreply.github.com>
2022-06-05 11:35:17 +0200

    Add files via upload

--- Metaballs/Metaballs.ino
+++ Metaballs/Metaballs.ino
@@ -7,11 +7,11 @@
 
 M5AtomDisplay display (WIDTH, HEIGHT);
 
-#define NUM_BALLS     5
+#define NUM_BALLS     6
 #define MIN_VELOCITY  2
-#define MAX_VELOCITY  5
-#define MIN_RADIUS    4
-#define MAX_RADIUS    8
+#define MAX_VELOCITY  7
+#define MIN_RADIUS    5
+#define MAX_RADIUS    10
 
 uint16_t coll;
 
@@ -29,11 +29,13 @@ struct ball {
 struct ball balls[NUM_BALLS];
 
 void metaballs_init(){
+
+  display.fillScreen(TFT_BLACK);
   
   for (int i = 0; i < NUM_BALLS; i++) {
     balls[i].radius = (esp_random() % MAX_RADIUS) + MIN_RADIUS;
-    balls[i].position.x = esp_random() % (WIDTH/8);
-    balls[i].position.y = esp_random() % (HEIGHT/8);
+    balls[i].position.x = esp_random() % (WIDTH/6);
+    balls[i].position.y = esp_random() % (HEIGHT/6);
     balls[i].velocity.x = (esp_random() % MAX_VELOCITY) + MIN_VELOCITY;
     balls[i].velocity.y = (esp_random() % MAX_VELOCITY) + MIN_VELOCITY;
   }
@@ -45,16 +47,16 @@ void metaballs_animate(){
   for (int i = 0; i < NUM_BALLS; i++) {
     balls[i].position.x += balls[i].velocity.x;
     balls[i].position.y += balls[i].velocity.y;
-    if ((balls[i].position.x < 0) | (balls[i].position.x > (WIDTH/8))) balls[i].velocity.x = balls[i].velocity.x * -1;
-    if ((balls[i].position.y < 0) | (balls[i].position.y > (HEIGHT/8))) balls[i].velocity.y = balls[i].velocity.y * -1;
+    if ((balls[i].position.x < 0) | (balls[i].position.x > (WIDTH/6))) balls[i].velocity.x = balls[i].velocity.x * -1;
+    if ((balls[i].position.y < 0) | (balls[i].position.y > (HEIGHT/6))) balls[i].velocity.y = balls[i].velocity.y * -1;
   }
   
 }
 
 void metaballs_render(){  
 
-  for (int y = 0; y < HEIGHT/8; y++) {
-    for (int x = 0; x < WIDTH/8; x++) {
+  for (int y = 0; y < HEIGHT/6; y++) {
+    for (int x = 0; x < WIDTH/6; x++) {
       float sum = 0;
       for (int i = 0; i < NUM_BALLS; i++) {
         float dx = x - balls[i].position.x;
@@ -68,11 +70,13 @@ void metaballs_render(){
       else if (sum > 0.35f) coll = TFT_YELLOW;
       else coll = TFT_BLACK;
 
-      display.writeFillRect(8*x, 8*y, 4, 4, coll);
+      display.writeFillRect(6*x, 6*y, 3, 3, coll);
             
     }
   }
 }
+
+static void on_button(void){ metaballs_init(); }
 
 void setup(void){
 
@@ -82,6 +86,9 @@ void setup(void){
   display.startWrite();
   display.setColorDepth(16);
   display.fillScreen(TFT_BLACK);
+
+  pinMode(39, INPUT);
+  attachInterrupt(digitalPinToInterrupt(39), on_button, FALLING);
 
   metaballs_init();
 
